9e9fe8a76345e2d3bb5832e4c70e6704b8ef646c
leo le leu <leohrc76@gmail.com>
2023-01-17 16:25:15 +0100

    modification script (not finished)

--- assets/javascript/jeux.js
+++ assets/javascript/jeux.js
@@ -1,19 +1,28 @@
-let options = ["pierre", "feuille", "ciseaux", "lézard", "spock"];
+let userChoice='';
 
-let playerChoice = prompt("Choisissez pierre, feuille, ciseaux, lézard ou spock :");
+document.getElementById("buttonId").addEventListener("click", function(){
+    userChoice=this.getAttribute('class');
+    console.log(userChoice);
+  });
+
+let options = ["rock", "paper", "scissor", "lizard", "spock"];
 
 let computerChoice = options[Math.floor(Math.random() * options.length)];
 
-if (playerChoice === computerChoice) {
-  console.log("Egalité !");
-} else if (
-  (playerChoice === "pierre" && (computerChoice === "ciseaux" || computerChoice === "lézard")) || 
-  (playerChoice === "feuille" && (computerChoice === "pierre" || computerChoice === "spock")) || 
-  (playerChoice === "ciseaux" && (computerChoice === "feuille" || computerChoice === "lézard")) || 
-  (playerChoice === "lézard" && (computerChoice === "feuille" || computerChoice === "spock")) || 
-  (playerChoice === "spock" && (computerChoice === "pierre" || computerChoice === "ciseaux"))
-) {
-  console.log("Vous avez gagné ! Votre choix était " + playerChoice + " et celui de l'ordinateur était " + computerChoice);
-} else {
-  console.log("Vous avez perdu ! Votre choix était " + playerChoice + " et celui de l'ordinateur était " + computerChoice);
+if (userChoice === ''){
+  console.log('not choosed yet');}
+else {
+    if (userChoice === computerChoice) {
+  console.log("Egalité !");} 
+    else if (
+    (userChoice === "rock" && (computerChoice === "scissor" || computerChoice === "lizard")) || 
+    (userChoice === "paper" && (computerChoice === "rock" || computerChoice === "spock")) || 
+    (userChoice === "scissor" && (computerChoice === "paper" || computerChoice === "lizard")) || 
+    (userChoice === "lizard" && (computerChoice === "paper" || computerChoice === "spock")) || 
+    (userChoice === "spock" && (computerChoice === "rock" || computerChoice === "scissor"))
+    ) {
+    console.log("Vous avez gagné ! Votre choix était " + userChoice + " et celui de l'ordinateur était " + computerChoice);
+    } else {
+    console.log("Vous avez perdu ! Votre choix était " + userChoice + " et celui de l'ordinateur était " + computerChoice);
+    }
 }
