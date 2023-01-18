de5875a8f2de9375034924a91315acc63d1417a6
boubalourson <boobaloursonZEBI@gmail.com>
2023-01-18 11:51:47 +0100

    rien

--- assets/javascript/jeux.js
+++ assets/javascript/jeux.js
@@ -8,6 +8,9 @@ let userChoice = "";
 
 // let cleanScore = localStorage.clear();
 
+UserScore = 0;
+computerScore = 0;
+
 document
   .getElementById("rock")
   .addEventListener("click", function (userChoice) {
@@ -94,7 +97,6 @@ if (userChoice === computerChoice) {
       " et celui de l'ordinateur était " +
       computerChoice
   );
-  cleanScore;
 } else {
   console.log(
     "Vous avez perdu ! Votre choix était " +
@@ -102,5 +104,4 @@ if (userChoice === computerChoice) {
       " et celui de l'ordinateur était " +
       computerChoice
   );
-  cleanScore;
 }
