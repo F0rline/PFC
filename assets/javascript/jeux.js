9c8417e584c9642ccd5d53907b81695af53fee52
Maxime Quertier <quertiermaxime76@gmail.com>
2023-01-18 14:00:34 +0100

    modif start button

--- assets/javascript/jeux.js
+++ assets/javascript/jeux.js
@@ -60,61 +60,58 @@ let options = ["rock", "paper", "scissor", "lizard", "spock"];
 let computerChoice = options[Math.floor(Math.random() * options.length)];
 console.log(computerChoice);
 
-var button = document.getElementById("launch");
-button.onclick = function () {
-  startGame();
-};
+var button = document.getElementById("launch").addEventListener("click", startGame());
 
 function startGame() {
 
-let options = ["rock", "paper", "scissor", "lizard", "spock"];
+  let options = ["rock", "paper", "scissor", "lizard", "spock"];
 
-let computerChoice = options[Math.floor(Math.random() * options.length)];
-console.log(computerChoice)
+  let computerChoice = options[Math.floor(Math.random() * options.length)];
+  console.log(computerChoice)
 
-if (computerChoice == "paper") {
-  document.getElementById("choise-computer").src = "/assets/img/paper.svg";
-  console.log(computerChoice);
-} else if (computerChoice == "rock") {
-  document.getElementById("choise-computer").src = "/assets/img/stone.svg";
-  console.log(computerChoice);
-} else if (computerChoice == "scissor") {
-  document.getElementById("choise-computer").src = "/assets/img/scissors.svg";
-  console.log(computerChoice);
-} else if (computerChoice == "lizard") {
-  document.getElementById("choise-computer").src = "/assets/img/lizard.svg";
-  console.log(computerChoice);
-} else if (computerChoice == "spock") {
-  document.getElementById("choise-computer").src = "/assets/img/spocke.svg";
-  console.log(computerChoice);
-}
+  if (computerChoice == "paper") {
+    document.getElementById("choise-computer").src = "/assets/img/paper.svg";
+    console.log(computerChoice);
+  } else if (computerChoice == "rock") {
+    document.getElementById("choise-computer").src = "/assets/img/stone.svg";
+    console.log(computerChoice);
+  } else if (computerChoice == "scissor") {
+    document.getElementById("choise-computer").src = "/assets/img/scissors.svg";
+    console.log(computerChoice);
+  } else if (computerChoice == "lizard") {
+    document.getElementById("choise-computer").src = "/assets/img/lizard.svg";
+    console.log(computerChoice);
+  } else if (computerChoice == "spock") {
+    document.getElementById("choise-computer").src = "/assets/img/spocke.svg";
+    console.log(computerChoice);
+  }
 
-  if (userChoice === computerChoice) {
-    console.log("Egalité !");
-  } else if (
-    (userChoice === "rock" &&
-      (computerChoice === "scissor" || computerChoice === "lizard")) ||
-    (userChoice === "paper" &&
-      (computerChoice === "rock" || computerChoice === "spock")) ||
-    (userChoice === "scissor" &&
-      (computerChoice === "paper" || computerChoice === "lizard")) ||
-    (userChoice === "lizard" &&
-      (computerChoice === "paper" || computerChoice === "spock")) ||
-    (userChoice === "spock" &&
-      (computerChoice === "rock" || computerChoice === "scissor"))
-  ) {
-    console.log(
-      "Vous avez gagné ! Votre choix était " +
-        userChoice +
-        " et celui de l'ordinateur était " +
-        computerChoice
-    );
-    } else {
-    console.log(
-      "Vous avez perdu ! Votre choix était " +
-        userChoice +
-        " et celui de l'ordinateur était " +
-        computerChoice
-    );
-    }
+    if (userChoice === computerChoice) {
+      console.log("Egalité !");
+    } else if (
+      (userChoice === "rock" &&
+        (computerChoice === "scissor" || computerChoice === "lizard")) ||
+      (userChoice === "paper" &&
+        (computerChoice === "rock" || computerChoice === "spock")) ||
+      (userChoice === "scissor" &&
+        (computerChoice === "paper" || computerChoice === "lizard")) ||
+      (userChoice === "lizard" &&
+        (computerChoice === "paper" || computerChoice === "spock")) ||
+      (userChoice === "spock" &&
+        (computerChoice === "rock" || computerChoice === "scissor"))
+    ) {
+      console.log(
+        "Vous avez gagné ! Votre choix était " +
+          userChoice +
+          " et celui de l'ordinateur était " +
+          computerChoice
+      );
+      } else {
+      console.log(
+        "Vous avez perdu ! Votre choix était " +
+          userChoice +
+          " et celui de l'ordinateur était " +
+          computerChoice
+      );
+      }
   }
